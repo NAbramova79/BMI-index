9ece3991debfc429ac0f577062d88e4a5a5241b4
NataA <nyabramova@gmail.com>
2023-06-11 17:31:05 +0200

    Index calc

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        }
+        BmiService service = new BmiService();
+        int weight = 62; // Значение веса в килограммах
+        double height = 1.64; // Значение роста в метрах
+        double bmi = service.calculate(height, weight);
+        
+        System.out.println("Значение индекса массы тела:");
+        System.out.println((int)bmi);
     }
 }
